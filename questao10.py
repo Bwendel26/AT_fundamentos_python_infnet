06d291bd3dbec5ce4777590fd9f6e4f33f515e45
brunoFernandes <bwendel26999@gmail.com>
2020-12-13 23:27:16 -0300

    ...

--- questao10.py
+++ questao10.py
@@ -12,3 +12,42 @@
 # NO FEMININO. SUA RESPOSTA DEVE IMPRIMIR UM RELATÓRIO MOSTRANDO O TOTAL DE
 # MEDALHAS DE CADA UM DOS PAÍSES E EM QUE ESPORTE, ANO, CIDADE E GÊNERO
 # (MASCULINO OU FEMININO) CADA MEDALHA FOI OBTIDA.
+
+import requests
+# import csv
+
+url = 'https://sites.google.com/site/dr2fundamentospython/arquivos/Winter_Olympics_Medals.csv'
+gold_medals = []
+total_medals = {}
+
+csv = requests.get(url).text
+csv_lines = csv.splitlines()
+
+for line in csv_lines:
+            coluna = line.split(',')
+            if coluna[0] != 'Year' and int(coluna[0]) >= 2001:
+                if coluna[4] == 'SWE' or coluna[4] == 'DEN' or coluna[4] == 'NOR':
+                    if coluna[2] == 'Curling' or coluna[2] == 'Skating' or coluna[2] == 'Skiing' or coluna[2] == 'Ice Hockey':
+                        if coluna[7] == 'Gold':
+                            gold_medals.append(coluna)
+
+
+def data_processed():
+
+    country_gold_medal = {'SWE': 0, 'DEN': 0, 'NOR': 0}
+    for country in gold_medals:
+        if country[4] == 'SWE':
+            country_gold_medal['SWE'] += 1
+        if country[4] == 'DEN':
+            country_gold_medal['DEN'] += 1
+        if country[4] == 'NOR':
+            country_gold_medal['NOR'] += 1
+    total_medals = country_gold_medal
+    return total_medals
+
+
+data_processed()
+
+print(total_medals)
+# print('The country with the most gold medals in the 21st century in Skiing,',
+#       'Curling, Skating, Ice Hockey was Norway with {} medals.'.format(total_medals['NOR']))
